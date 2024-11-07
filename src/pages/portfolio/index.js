f11e3de0e0b93fbe526cc180fa148b171390b189
SunnyDerwish <dalfeghali@gmail.com>
2024-11-07 17:05:38 -0500

    Fixed the flex boxes for the websites

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -7,31 +7,29 @@ import { dataportfolio, meta } from "../../content_option";
 export const Portfolio = () => {
   return (
     <HelmetProvider>
-      <Container className="About-header">
+      <Container>
         <Helmet>
           <meta charSet="utf-8" />
-          <title> Portfolio | {meta.title} </title>{" "}
+          <title> Portfolio | {meta.title} </title>
           <meta name="description" content={meta.description} />
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <h1 className="display-4 mb-4"> Portfolio </h1>
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
-        <div className="mb-5 po_items_ho">
-          {dataportfolio.map((data, i) => {
-            return (
-              <div key={i} className="po_item">
-                <img src={data.img} alt={data.title} />
-                <h3 className="portfolio-title">{data.title}</h3>
-                <div className="content">
-                  <p>{data.description}</p>
-                  <a href={data.link}>view project</a>
-                </div>
+        <div className="po_items_ho">
+          {dataportfolio.map((data, i) => (
+            <div key={i} className="po_item">
+              <img src={data.img} alt={data.title} />
+              <div className="portfolio-title">{data.title}</div>
+              <div className="content">
+                <p>{data.description}</p>
+                <a href={data.link}>view project</a>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       </Container>
     </HelmetProvider>
